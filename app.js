3e9d2d22634e2663dac04f2fa44ed3553a440914
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-04-01 06:00:39 +0900

    create operate function which takes the two value and an operator and does the math

--- app.js
+++ app.js
@@ -5,3 +5,17 @@ const subtact = (a,b) => a - b; //subtraction
 const multiply = (a,b) => a * b; //multiplication
 
 const divide = (a,b) => a/b ;  //division
+
+function operate(a, b, operator = "+") {
+    if (operator === "+") {
+      return add(a, b);
+    } else if (operator === "-") {
+      return subtract(a, b);
+    } else if (operator === "x") {
+      return multiply(a, b);
+    } else if (operator === "/") {
+      return divide(a, b);
+    } else {
+      throw new Error("Unsupported operator");
+    }
+  }
