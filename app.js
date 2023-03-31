2114b579673833fc7fb2cf5a637764d853c1df06
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-04-01 05:40:00 +0900

    create functions for addition,subtraction,multiplication and division

---
+++ app.js
@@ -0,0 +1,7 @@
+const add = (a,b) => a + b ;  //addition 
+
+const subtact = (a,b) => a - b; //subtraction
+
+const multiply = (a,b) => a * b; //multiplication
+
+const divide = (a,b) => a/b ;  //division
